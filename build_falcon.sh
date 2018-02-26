9fd0bccec3c93eb4878cd5740819a81eaa9b1af3
MSe <mse1969@posteo.de>
2018-02-26 19:01:09 +0100

    Adapt f. cm13-microG organization, add falcon
    Change-Id: Id47507feb6f2610ca23db19ad2a44d78295f1570

--- build_falcon.sh
+++ build_falcon.sh
@@ -5,10 +5,6 @@ export USE_CCACHE=1
 #export CCACHE_DIR=~/.ccache14
 prebuilts/misc/linux-x86/ccache/ccache -M 32G
 
-# Adjust TOOLCHAIN path 
-export KERNEL_TOOLCHAIN=$PWD/prebuilts/gcc/linux-x86/arm/linaro-arm-eabi-6.4/bin
-export KERNEL_TOOLCHAIN_PREFIX=arm-eabi-
-
 # with root
 export WITH_SU=true
 
@@ -20,6 +16,6 @@ export KBUILD_BUILD_USER=android
 export KBUILD_BUILD_HOST=localhost
 
 # Execute Brunch
-brunch osprey
+brunch falcon
 
 
---
+++ build_gts210ltexx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Initiate environment
+source build/envsetup.sh
+
+# CCache
+# ------
+# uncomment the below line to set own cache 
+# directory (default is ~/.ccache)
+export USE_CCACHE=1
+#export CCACHE_DIR=~/android/.ccache
+prebuilts/misc/linux-x86/ccache/ccache -M 32G
+
+# build with root baked in
+export WITH_SU=true
+
+# Normalize build metadata
+export KBUILD_BUILD_USER=android
+export KBUILD_BUILD_HOST=localhost
+
+# start build
+brunch gts210ltexx
+
