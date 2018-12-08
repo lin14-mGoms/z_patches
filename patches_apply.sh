de10a811ca1db966397c4aa5bceee7e7f0e978e8
MSe <mse1969@posteo.de>
2018-12-08 11:32:01 +0100

    Don't apply microG-patch in Lineage reference build
    Change-Id: Ia89be24db005235ed1603b0506d99d49d2724a76

--- patches_apply.sh
+++ patches_apply.sh
@@ -10,14 +10,6 @@ echo "Patching $PWD (user/host metadata)"
 patch -p1 < $THISDIR/patch_001_build.patch
 cd $TOPDIR
 
-cd frameworks/base
-echo "Patching $PWD (microG Patch)"
-patch -p1 < $THISDIR/patch_003_microG_001-N.patch
-patch -p1 < $THISDIR/patch_003_microG_002-N.patch
-cd core/res/res/values
-rm *orig
-cd $TOPDIR
-
 cd $THISDIR
 
 
