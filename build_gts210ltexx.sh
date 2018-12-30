c8114e82ca4b6244c95515e8a54b0d39a09b52eb
MSe <mse1969@posteo.de>
2018-12-30 16:47:42 +0100

    Patch: Signed builds for gts210ltexx
    Change-Id: I22269ecf9959d12dbe30511678da061010cbf7a3

--- build_gts210ltexx.sh
+++ build_gts210ltexx.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Check parameters
+case "$1" in
+  test) TESTKEY=true
+    ;;
+  sign) TESTKEY=false
+    ;;
+  *) echo "usage: build_gts210ltexx test|sign"
+     echo "----------------------------------"
+     echo "test - build with testkeys (insecure, but compatible)"
+     echo "sign - create a signed build"
+     exit
+    ;;   
+esac
+
 # Initiate environment
 source build/envsetup.sh
 
@@ -17,6 +31,22 @@ export WITH_SU=true
 # Normalize build metadata
 export KBUILD_BUILD_USER=android
 export KBUILD_BUILD_HOST=localhost
+
+if [ "$TESTKEY" = false ] ; then
+  export OWN_KEYS_DIR=~/.android-certs
+  export RELEASE_TYPE=UNOFFICIAL-microG-signed
+
+  # We need symlinks to fake the existence of a testkey 
+  # for the selinux build process and cyngn-priv-app for
+  # some old CM apps
+  for c in cyngn{-priv,}-app testkey; do
+    for e in pk8 x509.pem; do
+      ln -s releasekey.$e "$OWN_KEYS_DIR/$c.$e" 2> /dev/null
+    done
+  done
+else
+  export RELEASE_TYPE=UNOFFICIAL-microG
+fi
 
 # start build
 brunch gts210ltexx
--- patches_apply.sh
+++ patches_apply.sh
@@ -10,6 +10,11 @@ echo "Patching $PWD (user/host metadata)"
 patch -p1 < $THISDIR/patch_001_build.patch
 cd $TOPDIR
 
+cd vendor/cm
+echo "Patching $PWD (signed builds)"
+patch -p1 < $THISDIR/patch_002_vendor_cm.patch
+cd $TOPDIR
+
 cd $THISDIR
 
 
