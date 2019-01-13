2e7bec1ae2ecb20eb4bd7d324090f4ca78fd0a85
MSe <mse1969@posteo.de>
2019-01-13 11:50:07 +0100

    Apply microG-patch again in reference build and allow custom build names
    The microG patch should again also be applied to the reference build.
    Further, the reset option to rename all unofficial builds to UNOFFICIAL is
    removed in the vendor/cm patch to allow a custom file name containing the
    expression 'signed'.
    
    Change-Id: Ic1b4b9c0420365fd7ebd1bbc77ed6984f52b4911

--- patches_apply.sh
+++ patches_apply.sh
@@ -10,6 +10,14 @@ echo "Patching $PWD (user/host metadata)"
 patch -p1 < $THISDIR/patch_001_build.patch
 cd $TOPDIR
 
+cd frameworks/base
+echo "Patching $PWD (microG Patch)"
+patch -p1 < $THISDIR/patch_003_microG_001-N.patch
+patch -p1 < $THISDIR/patch_003_microG_002-N.patch
+cd core/res/res/values
+rm *orig
+cd $TOPDIR
+
 cd vendor/cm
 echo "Patching $PWD (signed builds)"
 patch -p1 < $THISDIR/patch_002_vendor_cm.patch
