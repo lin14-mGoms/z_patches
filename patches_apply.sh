2c424dd3d1f91db91d31bdf832a7ddcaf04ec843
MSe <mse1969@posteo.de>
2019-12-08 11:54:15 +0100

    Add 'p7zip hugefiles' patch
    Change-Id: I2b0951f21721ac1dcf296bbe5647b6a0737045db

--- patches_apply.sh
+++ patches_apply.sh
@@ -10,6 +10,11 @@ echo "Patching $PWD (user/host metadata)"
 patch -p1 < $THISDIR/patch_001_build.patch
 cd $TOPDIR
 
+cd external/p7zip
+echo "Patching $PWD (p7zip hugefile support)"
+patch -p1 < $THISDIR/patch_004_p7zip.patch
+cd $TOPDIR
+
 cd frameworks/base
 echo "Patching $PWD (microG Patch)"
 patch -p1 < $THISDIR/patch_003_microG_001-N.patch
