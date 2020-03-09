cb3148ce3f01817e964578a56e35a860b066d3e6
MSe <mse1969@posteo.de>
2020-03-09 12:02:11 +0100

    Revert "Add 'p7zip hugefiles' patch"
    This reverts commit 8e3b05b11d2ea4f783ade7e699baab2995e2f186.
    The commit has meanwhile been merged upstream, patch no longer needed.
    
    Change-Id: I1ca9e67a3afc6d916ffc174d3125d1e2d4fb7583

--- patches_apply.sh
+++ patches_apply.sh
@@ -10,11 +10,6 @@ echo "Patching $PWD (user/host metadata)"
 patch -p1 < $THISDIR/patch_001_build.patch
 cd $TOPDIR
 
-cd external/p7zip
-echo "Patching $PWD (p7zip hugefile support)"
-patch -p1 < $THISDIR/patch_004_p7zip.patch
-cd $TOPDIR
-
 cd $THISDIR
 
 
