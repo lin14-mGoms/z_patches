5ba5641c17d8e57069abbb34839cfde5bf507843
MSe <mse1969@posteo.de>
2020-04-29 11:42:28 +0200

    Osprey build script: Remove kernel toolchain adjustment
    This has been put into the osprey device configuartion, where it
    really belongs (the work-around here was mainly done to avoid having
    a fork of the device config repos, but as we DO have a fork already,
    why not take advantage of it?)
    
    Change-Id: Iace4c53a262706c5f96e164107b1ab5f3b7067b3

--- build_osprey.sh
+++ build_osprey.sh
@@ -5,17 +5,13 @@ export USE_CCACHE=1
 #export CCACHE_DIR=~/.ccache14
 prebuilts/misc/linux-x86/ccache/ccache -M 40G
 
-# Adjust TOOLCHAIN path 
-export KERNEL_TOOLCHAIN=$PWD/prebuilts/gcc/linux-x86/arm/arm-linux-gnueabi/bin
-export KERNEL_TOOLCHAIN_PREFIX=arm-linux-gnueabi-
-
 # with root
 export WITH_SU=true
 
 # Initiate env
 source build/envsetup.sh
 
-# Anonymize Kernel build
+# Normalize build metadata
 export KBUILD_BUILD_USER=android
 export KBUILD_BUILD_HOST=localhost
 
