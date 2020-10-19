4b24816f1edca1f24c4ead1665b4576fc0361ad9
MSe <mse1969@posteo.de>
2020-10-19 18:39:38 +0200

    Add emulator build script
    Change-Id: I4f727ba4b02bce4ad2eaa0bba37c6e5b2e9e2df6

---
+++ build_x86.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Initiate environment
+source build/envsetup.sh
+
+# CCache
+# ------
+# uncomment the below line to set own cache 
+# directory (default is ~/.ccache)
+export USE_CCACHE=1
+#export CCACHE_DIR=~/android/.ccache
+prebuilts/misc/linux-x86/ccache/ccache -M 40G
+
+export WITH_SU=true
+
+# microG build
+export RELEASE_TYPE=UNOFFICIAL-oms
+
+# Normalize build metadata
+export KBUILD_BUILD_USER=android
+export KBUILD_BUILD_HOST=localhost
+
+# start build
+lunch lineage_emulatorx86-userdebug
+mka
+
