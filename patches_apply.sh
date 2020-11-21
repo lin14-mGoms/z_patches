a85ff936621917789bce91c5f5a12137b8c532ed
MSe <mse1969@posteo.de>
2020-11-21 10:09:54 +0100

    Revert "Patch: Fix AOSP EMail widget"
    This reverts commit c9c7f7e1238f84a103f103ff8685ed6a4dd7dbee.
    
    Patch has been merged in LineageOS upstream.

--- patches_apply.sh
+++ patches_apply.sh
@@ -10,11 +10,6 @@ echo "Patching $PWD (user/host metadata)"
 patch -p1 < $THISDIR/patch_001_build.patch
 cd $TOPDIR
 
-cd packages/apps/Email
-echo "Patching $PWD (E-Mail Widget)"
-patch -p1 < $THISDIR/patch_010_Email.patch
-cd $TOPDIR
-
 cd $THISDIR
 
 
